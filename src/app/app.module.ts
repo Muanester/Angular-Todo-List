78509c0ed09a61b80c0fc537cd438fa732c70e10
Muanester <stevenmutisya999@gmail.com>
2023-12-22 04:20:25 +0300

    Complete app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, DashboardComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/component/dashboard/dashboard.component.ts
@@ -0,0 +1,78 @@
+import { Task } from '../../model/task';
+import { CrudService } from './../../service/crud.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css',
+})
+export class DashboardComponent {
+  taskObj: Task = new Task();
+  taskArr: Task[] = [];
+
+  addTaskValue: string = '';
+  editTaskValue: string = '';
+
+  constructor(private crudService: CrudService) {}
+
+  ngOnInit(): void {
+    this.addTaskValue = '';
+    this.editTaskValue = '';
+    this.taskObj = new Task();
+    this.taskArr = [];
+    this.getAllTask();
+  }
+
+  getAllTask() {
+    this.crudService.getAllTask().subscribe(
+      (res) => {
+        this.taskArr = res;
+      },
+      (err) => {
+        alert('Unable to get list of tasts' + err);
+      }
+    );
+  }
+
+  addTask() {
+    this.taskObj.task_name = this.addTaskValue;
+    this.crudService.addTask(this.taskObj).subscribe(
+      (res) => {
+        this.ngOnInit();
+        this.addTaskValue = '';
+      },
+      (err) => {
+        alert(err);
+      }
+    );
+  }
+
+  editTask() {
+    this.taskObj.task_name = this.editTaskValue;
+    this.crudService.editTask(this.taskObj).subscribe(
+      (res) => {
+        this.ngOnInit();
+      },
+      (err) => {
+        alert('Failed to update task' + err);
+      }
+    );
+  }
+
+  deleteTask(etask: Task) {
+    this.crudService.deleteTask(etask).subscribe(
+      (res) => {
+        this.ngOnInit();
+      },
+      (err) => {
+        alert('Failed to delete task' + err);
+      }
+    );
+  }
+
+  call(etask: Task) {
+    this.taskObj = etask;
+    this.editTaskValue = etask.task_name;
+  }
+}
